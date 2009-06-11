16a53fe8b89c73446aad101ee8df0c8f3859a55d
nika <nikiforov.al@gmail.com>
2009-06-12 00:49:53 +0400

    SRAM: fix for right timing of real chip

--- src/hardware/sram_M5M5V208FP-85L/sram.vhd
+++ src/hardware/sram_M5M5V208FP-85L/sram.vhd
@@ -1,10 +1,11 @@
--- sample from book
+-- controller M5M5V208FP-85L 
 
 library ieee;
 use ieee.std_logic_1164.all;
 entity sram_ctrl is
 	port(
 		clk, reset in std_logic;
+
 		-- to/from main system
 		mem: in std_logic;
 		rw: in std_logic;
@@ -12,8 +13,10 @@ entity sram_ctrl is
 		data_f2s: in std_logic_vector(7 downto 0);
 		ready: out std_logic;
 		data_s2f_r: data_s2f_ur: out std_logic_vector(7 downto 0);
+
 		-- to/from chip
 		address: out std_logic_vector(17 downto 0);
+
 		-- sram chip
 		dio_a: inout std_logic_vector(7 downto 0);
 		s1, s2: out std_logic
@@ -43,14 +46,15 @@ begin
 			tri_reg <= '1';
 			we_reg <= '1';
 			oe_reg <= '1';
-		elsif (clk'event and clk = '1')		-- FIXME to rising edge
+		--elsif (clk'event and clk = '1')		-- FIXME to rising edge
+		elsif rising_edge(clk)		
 			state_reg <= state_next;
 			addr_reg <= addr_next;
 			data_f2s_reg <= data_f2s_next;
 			data_s2f_reg <= data_s2f_next;
 			tri_reg <= tri_buf;
 			we_reg <= we_buf;
-			oe_req <= oe_buf;
+			oe_reg <= oe_buf;
 		end if;
 	end process;
 
@@ -61,31 +65,50 @@ begin
 		data_f2s_next <= data_s2s_reg;
 		data_s2f_next <= data_s2f_reg;
 		ready <= '0';
+
 		case state_reg is
 			when idle =>
 				if mem = '0' then
 					state_next <= 'idle';
 				else 
 					addr_next <= addr;
-					if rw='0' then		-- write
+
+					if rw='1' then		-- write
 						state_next <= wr1;
 						data_f2s_next <= data_f2s;
+
 					else 			--read
 						state_next <= rd1;
 					end if;
+
 				end if;
 					ready <= '1';
 
+			-- 5 cycles - write cycle
+			-- setup address and we/oe 40ns > 30 ns
 			when wr1 =>
 				state_next <= wr2;
-
 			when wr2 =>
+				state_next <= wr3;
+			-- setup data on bus 40ns > 35 ns
+			when wr3 =>
+				state_next <= wr4;
+			when wr4 =>
+				state_next <= wr5;
+			-- clear the WE signal 20ns > 5ns
+			when wr5 =>
 				state_next <= idle;
 
+			-- 5 cycles of 20ns = 100ns > 85 ns - read cycle
 			when rd1 =>
 				state_next <= rd2;
-
-			when rd2 => 
+			when rd2 =>
+				state_next <= rd3;
+			when rd3 =>
+				state_next <= rd4;
+			when rd4 =>
+				state_next <= rd5;
+			when rd5 => 
 				data_s2f_next <= dio_a;
 				state_next <= idle;
 		end case;
@@ -101,16 +124,30 @@ begin
 		case state_next is
 			when idle =>
 			when wr1 =>
-				tri_buf <= '0';
 				we_buf <= '0';
-
-			when wr2 =>
 				tri_buf <= '0';
-
+			when wr2 =>
+				we_buf <= '0';
+				tri_buf <= '0';
+			when wr3 =>
+				we_buf <= '0';
+				tri_buf <= '0';
+			when wr4 =>
+				we_buf <= '0';
+				tri_buf <= '0';
+			when wr5 =>
+				tri_buf <= '0';
+			
+			-- read cycle
 			when rd1 =>
 				oe_buf <= '0';
-			
 			when rd2 =>
+				oe_buf <= '0';
+			when rd3 =>
+				oe_buf <= '0';
+			when rd4 =>
+				oe_buf <= '0';
+			when rd5 =>
 				oe_buf <= '0';
 		end case;
 	end process;
