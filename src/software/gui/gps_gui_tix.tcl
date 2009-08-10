477391254c9e97f35bcd9385c5ad4c11858c7369
nika <nikiforov.al@gmail.com>
2009-08-10 23:41:20 +0400

    gui: first version of a testing part

--- src/software/gui/gps_gui_tix.tcl
+++ src/software/gui/gps_gui_tix.tcl
@@ -26,23 +26,59 @@ proc init_settings {root_window note_book} {
 
 	label 	$rbt.rs232_l -text "Port name: " -padx 2
 	entry 	$rbt.rs232_e -width 40 -textvar rs232_name -border 1
+	
+	label 	$rbt.server_name_l -text "Server name: " -padx 2
+	entry 	$rbt.server_name_e -width 30 -textvar server_name -border 1
+	label 	$rbt.server_port_l -text "Server port: " -padx 2
+	entry 	$rbt.server_port_e -width 10 -textvar server_port -border 1
+
 	button 	$rbt.rs232_b -text "Connect" -command exit
 
-	# results
-	label $res.port -text "Unconnected" -padx 1
+	# headers 
+	label 	$rbt.set_l -text "Settings: " -padx 2 -font {Helvetica 14 bold}
+	label 	$rbt.test_l -text "Tests: " -padx 2 -font {Helvetica 14 bold}
 
-    	pack $res -side right -fill both 
-    	pack $rbt -side top -fill both
+	# test labels
+	label 	$rbt.mem_test_l -text "Memory test  ................................................................................................... " -padx 2
+	label 	$rbt.gps_test_l -text "GPS test  ......................................................................................................... " -padx 2
+
+	# results
+	label $res.port -text "UNCONNECTED" -padx 1
+	label $res.mem -text "UNTESTED" -padx 1
+	label $res.gps -text "UNTESTED" -padx 1
+
+	# ================================================
+	# place
+	# ================================================
+	# frames
+   	place $rbt -rely .0 -relx .0 -relheight 1 -relwidth .8 
+   	place $res -rely .0 -relx .8 -relheight 1 -relwidth .2 
+	
+	# headers 
+	place $rbt.set_l -relx 0.1 -rely 0
+	place $rbt.test_l -relx 0.1 -rely 0.2
+
+	# settings
+	place $rbt.server_name_l -relx 0 -rely 0.08
+	place $rbt.server_name_e -relx 0.18 -rely 0.08
+	place $rbt.server_port_l -relx 0.65 -rely 0.08
+	place $rbt.server_port_e -relx 0.82 -rely 0.08
+
+	place $rbt.rs232_l -relx 0 -rely 0.14
+	place $rbt.rs232_e -relx 0.15 -rely 0.14
+	
+	place $rbt.rs232_b -relx 0.8 -rely 0.85
+	
+	#tests
+	place $rbt.mem_test_l -relx 0 -rely 0.27 
+	place $rbt.gps_test_l -relx 0 -rely 0.32
+
+	# results
+	place $res.port -relx 0.5 -x -50 -rely 0.14
+	place $res.mem -relx 0.5 -x -37 -rely 0.27
+	place $res.gps -relx 0.5 -x -37 -rely 0.32
 
 	#$rbt.close configure -state disabled
-
-	#pack $rbt.rs232_l $rbt.rs232_e -side top -padx 5 -pady 5 -fill x
-	pack $rbt.rs232_l -side left -padx 2 -pady 2
-	pack $rbt.rs232_e -side left -padx 2 -pady 2 -fill x
-	pack $rbt.rs232_b -side right -padx 2 -pady 2
-
-	pack $res.port -padx 2 -pady 8 
-
 }
 
 
