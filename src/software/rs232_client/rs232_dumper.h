497bdd00bc7143064123cd39801b8a6953f2903a
nika <nikiforov.al@gmail.com>
2009-08-13 01:27:15 +0400

    rs232: work on dumper networks part for gui, implement base network FSM

--- src/software/rs232_client/rs232_dumper.h
+++ src/software/rs232_client/rs232_dumper.h
@@ -4,6 +4,16 @@
 #define MAXLINE		255
 #define BUF_SIZE	1024*1024	// 1 Mb
 
+/* commands */
+#define CONNECTION_CMD "HELLO_GPS_BOARD"
+
+enum rs232_fsm_state {
+	BREAK,			/* exit from cycle */
+	CONNECTION,
+	IDLE,
+	SET_PORT
+};
+
 enum rs232_comm_request {
 	RS232_SET_REG		= 1<<0,
 	RS232_TEST_MEMORY	= 1<<1,
@@ -23,13 +33,20 @@ struct rs232_data_s {
 
 	char		name[MAXLINE];
 	int		fd;
-	uint8_t 	recv_buf[BUF_SIZE];	
+	uint8_t 	recv_buf[BUF_SIZE];
 
 	uint64_t	comm_req;			// request comm
 
 	/* set register part */
 	uint64_t	reg;				/* register 27 bits */
 	uint64_t	addr;				/* address of register 4 bits */
+
+	/* network part */
+	int		sock;				/* listen socket */
+	int		csock;				/* connection socket */
+	uint16_t	port;
+	size_t		todo;
+	size_t		done;
 
 };
 
