b47e6fdb938e6322e14072fec3a19b382709d20a
nika <nikiforov.al@gmail.com>
2009-08-14 01:11:32 +0400

    gui: clenup the log module and improve network

--- src/software/gui/logs.tcl
+++ src/software/gui/logs.tcl
@@ -13,17 +13,24 @@ proc create_logs {root_window note_book} {
 	#scrollbar .srl_x -command ".txt xview" -orient h
 	
     	set logs_f [frame $w.logs_f]
-	set logs_win [text $logs_f.logs_win -width 80 -height 20 -yscrollcommand "$logs_f.scroll_y set" ]
-				#-xscrollcommand {$w.scroll_x set} ]
+	set logs_win [text $logs_f.logs_win	-width 80 \
+						-height 20 \
+						-yscrollcommand "$logs_f.scroll_y set" \
+						-xscrollcommand "$logs_f.scroll_x set" ]
 
-	scrollbar $logs_f.scroll_y -command "$logs_f.logs_win yview"
-	#scrollbar $w.scroll_x -command {$logs_win xview}
 
-	pack $logs_f.scroll_y -side right -fill y
+	#FIXME xscroll dontwork ???
+	scrollbar $logs_f.scroll_y -command "$logs_f.logs_win yview" -orient v
+	scrollbar $logs_f.scroll_x -command "$logs_f.logs_win xview" -orient h
+
+
 	place $logs_f -rely 0. -relx .0 -relheight 1 -relwidth 1 
 	place $logs_win -rely 0. -relx .0 -relheight 1 -relwidth 1 
 
-	tracep "create logs"
+	pack $logs_f.scroll_y -side right -fill y
+	pack $logs_f.scroll_x -side bottom -fill x
+
+	#tracep "create logs"
 }
 
 proc show_logs {root_window note_book} {
@@ -33,17 +40,19 @@ proc show_logs {root_window note_book} {
     	set logs_f	$w.logs_f
     	set logs_win	$logs_f.logs_win
 
-	tracep "raise logs"
+	#tracep "raise logs"
 	flush $log
 	
+	$logs_win configure -state normal
+
 	set fl [open log]
 	$logs_win delete 1.0 end
 	set buffer [read $fl]
-	puts $buffer
+	#puts $buffer
 	$logs_win insert insert $buffer\n
 	close $fl
 
 	$logs_win configure -state disable
 
-	puts "=============================================================="
+	#puts "=============================================================="
 }
