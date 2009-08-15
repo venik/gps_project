7eef226da8828f95867edc6d273a2bcdd5b5dcdf
z-force <z-force@bk.ru>
2009-08-15 16:24:35 +0400

    ca_generate(c version): Generate PRN for one satellite

--- src/software/ca_generator/ca_generate.c
+++ src/software/ca_generator/ca_generate.c
@@ -6,31 +6,38 @@
 
 int main()
 {
- int i,j,k,NumSat;
+ int i,j,k,NumSat,Length;
  int G1[]={0,0,0,0,0,0,0,0,0,0};
  int G2[]={0,0,0,0,0,0,0,0,0,0}; 
  int k1[] = {2, 3, 4, 5, 1,  2, 1, 2,  3, 2, 3, 5, 6, 7, 8,  9, 1, 2, 3, 4, 5, 6, 1, 4, 5, 6, 7,  8, 1, 2, 3, 4,  5,  4, 1, 2, 4 } ;
  int k2[] = {6, 7, 8, 9, 9, 10, 8, 9, 10, 3, 4, 6, 7, 8, 9, 10, 4, 5, 6, 7, 8, 9, 3, 6, 7, 8, 9, 10, 6, 7, 8, 9, 10, 10, 7, 8, 10 } ;
- int ResBit[]={0};
-k=0;
-NumSat=1;
-init(G1, G2);
+ k=0;
+  printf("Input number satellite ");
+  scanf("%d",&NumSat);
+  
+  printf("Input length string ");
+  scanf("%d",&Length);
+ int ResBit[Length];
+init(G1,G2);
+  for (k=0;k<Length;k++)
+{
+ResultBit(G1,G2,ResBit,k,NumSat,k1,k2);
 rotateg1(G1);
 rotateg2(G2);
-ResultBit(G1,G2,ResBit,k,NumSat);
+}
  for(i=0;i<N;i++)
  {
   printf("G1[%d]=%d \n",i+1, G1[i]);
-  printf("G2[%d]=%d \n",i+1, G2[i]);
  }
 
- for(j=0;j<M;j++)
+for (i=0;i<N;i++)
+{ printf("G2[%d]=%d \n",i+1, G2[i]);
+}
+
+ for(j=0;j<Length;j++)
  {
-  //printf("k1[%d]=%d \n", j+1, k1[j]);
-  //printf("k2[%d]=%d \n", j+1, k2[j]);
+ printf("ResBit[%d]=%d \n", j+1, ResBit[j]);
  }
-
- printf("ResBit[%d]=%d \n", 0, ResBit[1]);
 }
 
 
@@ -59,9 +66,9 @@ void rotateg1(int G1[n])
 
 void rotateg2(int G2[n])
 {
+  int G2Temp;
  int i;
- int G2Temp;
- G2Temp=(G2[9]+G2[8]+G2[7]+G2[5]+G2[2]+G2[1])%2;
+G2Temp=(G2[9]+G2[8]+G2[7]+G2[5]+G2[2]+G2[1])%2;
   for (i=9;i>0;i--)
   {
    G2[i]=G2[i-1];
@@ -69,7 +76,9 @@ void rotateg2(int G2[n])
  G2[0]=G2Temp;
 }
 
-void ResultBit(int G1[n], int G2[n], int ResBit[], int k, int NumSat)
+void ResultBit(int G1[n], int G2[n], int ResBit[], int k, int NumSat, int k1[], int k2[])
 {
- ResBit[k]=(G1[9]+G2[k1[NumSat]]+G2[k2[NumSat]])%2;
+ int out1=k1[NumSat-1];
+ int out2=k2[NumSat-1]; 
+ ResBit[k]=(G1[9]+G2[out1-1]+G2[out2-1])%2;
 }
