b58e9a621ef7102ab75e8fb72a7b4382c6757c2d
Aleksey Melnikov <melnikov.aleksey@gmail.com>
2009-09-17 21:59:23 +0300

    board: update values

--- src/hardware/board/xilinx_lab3/mscripts/ldo.m
+++ src/hardware/board/xilinx_lab3/mscripts/ldo.m
@@ -3,7 +3,7 @@ clc, clear all ;
 %Vout = 2.85 ;   % required output
 Vout = 1.8 ;   % required output
 
-R1 = 240 ;      % (see. LM117.pdf)
+R1 = 330 ;      % (see. LM117.pdf)
 Vref = 1.25 ;   % (see LM117.pdf, mc33269-d.pdf)
 Iadj = 100e-6 ; % (see LM117.pdf, mc33269-d.pdf)
 %Iadj = 0 ;
@@ -11,7 +11,8 @@ Iadj = 100e-6 ; % (see LM117.pdf, mc33269-d.pdf)
 R2 = (Vout - Vref)/(Vref/R1+Iadj) ;
 
 % check part
-R_2 = round(R2/100)*100 ;
+%R_2 = round(R2/100)*100 ;
+R_2 = ceil(R2/10)*10 ;
 V_out = Vref*(1+R_2/R1)+Iadj*R_2 ; 
 
 fprintf('R1 = %5.1f Om\nR2 = %5.1f Om\nOutput is %5.4f v\n', ...
