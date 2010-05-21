3ccbb52457ccd2ab7010579f65150a25695f44f1
nika_vocord <nikiforov.al@gmail.com>
2010-05-21 20:18:09 +0400

    gpstool: costas

--- src/software/gpstool/main2_fine_freq.m
+++ src/software/gpstool/main2_fine_freq.m
@@ -1,6 +1,6 @@
 % main script for sattelite signal processing
 clc, clear all ;
-nDumpSize = 16368*7 ; 
+nDumpSize = 16368*16 ;          % FIXME - we can more 
 x = readdump('./data/flush',nDumpSize) ;
 %pwelch(x(1:16368),[],[],[],16.368e6) ;
 FR = 4092-5:1:4092+5 ; % frequency range kHz
@@ -15,8 +15,8 @@ max_fine_sat_new = zeros(32,1) ;
 sat_shift_ca = zeros(32,1) ;
 max_sat_freq = zeros(32,1) ;
 
-%PRN_range = 6 ;
-PRN_range = 1:32 ;
+PRN_range = 3 ;
+%PRN_range = 1:32 ;
 
 for PRN=PRN_range
     for f0 = FR
@@ -51,7 +51,7 @@ for PRN=PRN_range
     end
     
     for i=[1:3]
-        local_replica = exp(j*2*pi * fr(i)/fs * (0:N-1)) ;         % FIXME - minus
+        local_replica = exp(j*2*pi * fr(i)/fs * (0:N-1)) ;
         max_bin_freq(i) = abs( sum(data_5ms(1:N).' .* local_replica) ) ;
     end
    
@@ -66,14 +66,15 @@ for PRN=PRN_range
     phase = diff(-angle(sum(reshape(sig, N, 5))));
     phase_fix = phase;
     
-    threshold = 2.3*pi\5 ; % FIXME / or \
+    threshold = 2.3*pi/5 ; % FIXME / or \
     
     for i=1:4 ;
         if(abs(phase(i))) > threshold ;
             phase(i) = phase_fix(i) - 2*pi ;
             if(abs(phase(i))) > threshold ;
                 phase(i) = phase_fix(i) + 2*pi ;
-                if(abs(phase(i))) > 2.2*pi \ 5 ;    % FIXME / or \
+                %if(abs(phase(i))) > 2.2*pi / 5 ;    % FIXME / or \
+                if(abs(phase(i))) > threshold ;
                     phase(i) = phase_fix(i) - pi ;
                     if(abs(phase(i))) > threshold ;
                         phase(i) = phase_fix(i) - 3*pi ;
@@ -93,13 +94,33 @@ for PRN=PRN_range
     max_fine_sat_new(PRN) = abs( sum(data_5ms(1:N).' .* exp(j*2*pi * fr(shift_ca)/fs * (0:N-1) ) ) ) ;
         
     fprintf('#PRN: %2d CR: [%10.5f] CR+phi: [%10.5f] FREQ %10.5f phase: %f \n', PRN, max_fine_sat(PRN), max_fine_sat_new(PRN), fr(shift_ca), dfrq) ;
+   
+    % prepare for tracking
+    max_sat_freq(PRN) = fr(shift_ca) ;
     
 end
 
 fprintf('Results: \n') ;
 
-
 % plot result
-subplot(3, 1, 1), barh(max_fine_sat_new), xlim([0,11000]), colormap summer, grid on, title('Correlator outputs after fine freq estimation') ;
-subplot(3, 1, 2), barh(max_fine_sat), xlim([0,11000]), colormap summer, grid on, title('Correlator outputs with 400 Hz adjust') ;
-subplot(3, 1, 3), barh(max_sat),colormap summer, grid on, title('Correlator outputs') ;
+%subplot(3, 1, 1), barh(max_fine_sat_new), xlim([0,11000]), colormap summer, grid on, title('Correlator outputs after fine freq estimation') ;
+%subplot(3, 1, 2), barh(max_fine_sat), xlim([0,11000]), colormap summer, grid on, title('Correlator outputs with 400 Hz adjust') ;
+%subplot(3, 1, 3), barh(max_sat),colormap summer, grid on, title('Correlator outputs') ;
+
+% costas
+sec = 15;
+costas_phase = zeros(sec,1) ;
+for PRN=PRN_range
+    data = work_data(sat_shift_ca(PRN): sat_shift_ca(PRN) + sec*N-1);
+    
+    for data_step=0:sec-1
+        carrier_generator = exp(j*2*pi * max_sat_freq(PRN)/fs * (0:N-1)) ;
+        for_descr = data((data_step * N) + 1: (data_step+1)*N ) ;
+        
+        costas_phase(data_step + 1) = angle(sum(for_descr.' .* carrier_generator)) ;
+        dfrq = costas_phase(data_step + 1) / (2*pi) ;
+        max_sat_freq(PRN) =  max_sat_freq(PRN) + dfrq ;
+    end
+end
+
+fprintf('hello\n') ;
