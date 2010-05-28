d13661681c53aefbd7d1a869257b8973407db025
nika_vocord <nikiforov.al@gmail.com>
2010-05-28 18:33:15 +0400

    gpstool: fix costas model

--- src/software/gpstool/costas_pll2mhz.m
+++ src/software/gpstool/costas_pll2mhz.m
@@ -1,10 +1,11 @@
 clc, clear all ;
-sig_length = 1024000 ; % signal length
-fx = 2000000 ; % input signal frequency
+sig_length = 1024000*3 ; % signal length
+fx = 4092000 ; % input signal frequency
 fs = 16368000 ; % sampling frequency
 x = sin(2*pi * fx/fs * (0:sig_length-1)) ; % input signal
 
-f_nco_base = 2000010 ;
+fx_error = -10 ;
+f_nco_base = fx + fx_error ;
 data_mixed_I = zeros(sig_length,1) ;
 data_mixed_Q = zeros(sig_length,1) ;
 data_lpf_I = zeros(sig_length,1) ;
@@ -27,10 +28,10 @@ lpf_a = 0.0095 ;
 
 % loop filter constants - tsui related values
 ts = 1/fs ;
-zu_cl = 0.7 ;             % damping ratio
+zu_cl = 0.707 ;             % damping ratio
 %zu_cl = 1 ;             % damping ratio
 k0k1_cl = 4*pi * 100 ;      % gain
-B_cl = 20 ;                 % noise bandwith
+B_cl = 25 ;                 % noise bandwith
 omega_cl = 8*zu_cl*B_cl / (4*zu_cl^2 + 1) ;
 C1_cl = (1/k0k1_cl) * (8*zu_cl*omega_cl*ts / (4 + 4*zu_cl*omega_cl*ts + (omega_cl*ts)^2) ) ;
 C2_cl = (1/k0k1_cl) * (4*(omega_cl*ts)^2   / (4 + 4*zu_cl*omega_cl*ts + (omega_cl*ts)^2) ) ;
@@ -41,7 +42,7 @@ x_theta_cl = zeros(3,1) ;
 
 
 for n=1:sig_length
-    nco_sample = exp(j*2*pi*(f_nco_base+nco_ctl)/fs*(n-1)) ;
+    nco_sample = exp(j*2*pi*(f_nco_base - nco_ctl)/fs*(n-1)) ;
     mixer_I = x(n)*real(nco_sample) ;
     mixer_Q = x(n)*imag(nco_sample) ;
 
@@ -71,7 +72,7 @@ for n=1:sig_length
     
     lf_theta = y_theta_cl(3) ;
     
-    nco_ctl = lf_theta*80 ;    
+    nco_ctl = lf_theta * (9) ;    
     
     % collect data
     data_mixed_I(n) = mixer_I ;
@@ -83,6 +84,8 @@ for n=1:sig_length
     data_nco_ctl(n) = nco_ctl ;
     
 end
+
+fprintf(' base %f, error %f, adjusted %f \n', fx, fx_error, fx + data_nco_ctl(n));
 
 %figure(1),hold off, plot(data_mixed_Q,'r-'),hold on,plot(data_lpf_Q)
 %figure(2), plot(data_theta), grid on
