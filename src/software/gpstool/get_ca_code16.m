265ed4bd50f9e41c8ccdec682b9dab1a3ca12796
nika_vocord <nikiforov.al@gmail.com>
2010-07-17 19:42:34 +0400

    gpstool: fix comments

--- src/software/gpstool/get_ca_code16.m
+++ src/software/gpstool/get_ca_code16.m
@@ -1,13 +1,14 @@
+function ca16 = get_ca_code16(N,PRN)
 % /* get_ca_code16(N,PRN) */
 % /* function computes N values of C/A code 
 %                   resampled to 16.368MHz*/
 % /* N - number of chip values */
 % /* PRN  - sattelite code */
 % /* Status: is not tested */
-function ca16 = get_ca_code16(N,PRN)
+
 ca = get_ca_code(N+1,PRN) ; 
-chip_width = 1/1.023e6 ; % /* CA chip duration, sec */
-ts = 1/16.368e6 ; % /* discretization period, sec */
+chip_width = 1/1.023e6 ;                % /* CA chip duration, sec */
+ts = 1/16.368e6 ;                       % /* discretization period, sec */
 ca16 = zeros(N*16,1) ;
 for k=1:N*16
     ca16(k) = ca(ceil(ts*k/chip_width)) ;
